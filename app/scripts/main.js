282463ea49342153139a1fea7e2f234e49ac428b
felipemaciel <felipemrvieira@gmail.com>
2018-03-31 12:12:29 -0300

    ajustes no scroll do menu e smooth scroll nos links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 120) {
+            $('#menu-header').addClass('fixed');
+            console.log('adicionou');
+        } else {
+            $('#menu-header').removeClass('fixed');
+            console.log('removeu');
+        }
+    });
+    $(".scrollTo").on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+          scrollTop: ($(target).offset().top -60)
+        }, 500);
+     });
+});
+
+
