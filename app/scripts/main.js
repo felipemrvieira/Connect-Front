4d1f6591e5ba33ce2d4e82ff5515447031af8c0c
felipemaciel <felipemrvieira@gmail.com>
2018-03-31 12:32:49 -0300

    ajustes no offset do scroll ao variar o taanho do menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,19 +2,25 @@ $(document).ready(function () {
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 120) {
             $('#menu-header').addClass('fixed');
-            console.log('adicionou');
         } else {
             $('#menu-header').removeClass('fixed');
-            console.log('removeu');
         }
     });
-    $(".scrollTo").on('click', function(e) {
+    $(".scrollTo").on('click', function (e) {
         e.preventDefault();
         var target = $(this).attr('href');
+        var elemento = $('#menu-header').hasClass('fixed');
+    if (elemento) {
         $('html, body').animate({
-          scrollTop: ($(target).offset().top -60)
+            scrollTop: ($(target).offset().top - 60)
         }, 500);
-     });
+    } else {
+        $('html, body').animate({
+            scrollTop: ($(target).offset().top - 170)
+        }, 500);
+    }
+
+});
 });
 
 
